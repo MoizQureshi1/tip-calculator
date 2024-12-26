a63abc399ad11260dc9f027ca09f8ce298777148
Moiz Qureshi <m.moiz782008@gmail.com>
2024-12-26 16:22:43 +0500

    update
    push

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import TipCalculator from '@/components/tip-calculator'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <TipCalculator/>
+      </div>
+    </>
+  )
+}
